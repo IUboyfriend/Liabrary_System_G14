d9b69d14c1ffe4c4557d40af979b5db8470f115d
HAO Jiadong <20084595d@connect.polyu.hk>
2022-11-10 11:01:30 +0800

    表格修改后的search book

--- src/Controller/BookController.java
+++ src/Controller/BookController.java
@@ -7,17 +7,17 @@ public class BookController {
 
     public static ResultSet searchAll(String searchText,Controller.OracleDB oracleDB) throws SQLException {
 
-        String query = "SELECT * FROM BOOK WHERE \"BookName\" LIKE '%" + searchText + "%'" +
-                "OR \"Author\" LIKE '%" + searchText + "%'" +
-                "OR \"Category\" LIKE '%" + searchText + "%'"+
-                "OR \"Publisher\" LIKE '%" + searchText + "%'";
+        String query = "SELECT * FROM BOOK WHERE BookName LIKE '%" + searchText + "%'" +
+                "OR Author LIKE '%" + searchText + "%'" +
+                "OR Category LIKE '%" + searchText + "%'"+
+                "OR Publisher LIKE '%" + searchText + "%'";
         ResultSet rset = oracleDB.executeQuery(query);
         return rset;
     }
     public static ResultSet searchOneField(String searchText, String searchField, Controller.OracleDB oracleDB) throws SQLException {
         searchField = searchField.equals("Name")? "BookName":searchField;
 
-        String query = "SELECT * FROM BOOK WHERE \"" + searchField + "\" LIKE '%" + searchText + "%'";
+        String query = "SELECT * FROM BOOK WHERE " + searchField + " LIKE '%" + searchText + "%'";
         ResultSet rset = oracleDB.executeQuery(query);
         return rset;
     }
